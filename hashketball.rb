afe480d8fd78daff6f6fa2c78618dbf6f777fa9f
Terranie Sims <terranie67@gmail.com>
2020-09-22 19:21:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,13 @@
-def game_hash ={}
+def game_hash = {
+  :home => {
+  :team_name => "Brooklyn Nets",
+  :colors => ["Black", "White"],
+  :players => [{}], 
+},
+  :away => {
+  :team_name => "Charolotte Hornets",
+  :colors => ["Turquoise, Purple"],
+  :players => , 
+}}
   
 end 
