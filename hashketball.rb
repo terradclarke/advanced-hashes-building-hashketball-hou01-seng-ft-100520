fce854c54baf9cc85c8748808ff9445e669dd88c
Terranie Sims <terranie67@gmail.com>
2020-09-22 19:26:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,12 +2,34 @@ def game_hash = {
   :home => {
   :team_name => "Brooklyn Nets",
   :colors => ["Black", "White"],
-  :players => [{}], 
+  :players => [
+    {:player_name => , 
+    :number => , 
+    :shoe => , 
+    :points => , 
+    :rebounds => , 
+    :assists => , 
+    :steals => , 
+    :blocks => , 
+    :slam_dunks => }, 
+      
+    ], 
 },
   :away => {
   :team_name => "Charolotte Hornets",
   :colors => ["Turquoise, Purple"],
-  :players => , 
+  :players => , [
+     {:player_name => , 
+    :number => , 
+    :shoe => , 
+    :points => , 
+    :rebounds => , 
+    :assists => , 
+    :steals => , 
+    :blocks => , 
+    :slam_dunks => }, 
+    
+    ]
 }}
   
 end 
